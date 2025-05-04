800c16491a1f10167c7b00e7f17e130e3a4ff872
Jean-Christian Denis <contact@jcdenis.fr>
2025-05-04 13:21:54 +0200

    add french translation

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,22 @@
+<?php
+// Language: Français
+// Module: ReadingTracking - 0.1
+// Date: 2025-05-04 11:21:26
+// Author: , contact@jcdenis.fr
+// Translated with Translater - 2025.03.03
+
+
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Enable user reading tracking'] = 'Activer le suivi de lecture pour les utilisateurs';
+L10n::$locales['Artifact to use:'] = 'Artefact à utiliser :';
+L10n::$locales['This add artifact at the begining of unread posts titles. Or you can use custom templates instead.'] = 'Ajoute un artefact avant le titre d\'un billet non lu. Sinon vous pouvez utiliser les balises de templates.';
+L10n::$locales['Reading tracking'] = 'Suivi de lecture';
+L10n::$locales['Add artifact on unread entries'] = 'Ajouter l\'artefact aux billets non lus';
+L10n::$locales['Reset a post reading tracking on new comment'] = 'Réinitialiser le suivi de lecture d\'un billet lors d\'un nouveau commentaire';
+L10n::$locales['Mark all entires as read'] = 'Marquer tous les billets comme lus';
+L10n::$locales['Do not use artifact'] = 'Ne pas utiliser l\'artefact';
