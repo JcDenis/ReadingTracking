aa0453ab6c961f909434a443a64bf3ce73df3840
Jean-Christian Denis <contact@jcdenis.fr>
2025-05-14 15:32:11 +0200

    release 0.6.2

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Reading tracking',
     'Mark post as read for connected users.',
     'Jean-Christian Paul Denis and Contributors',
-    '0.6.1',
+    '0.6.2',
     [
         'requires'    => [
             ['core', '2.34'],
